ea811d1fb735085b4df2b495bdb823a42da5f6ae
guifdsk <gui.fdsk@gmail.com>
2022-10-31 23:51:38 -0300

    exercices: hackerrank nivel facil - #2022-10-31

---
+++ nivel-facil/src/exercicio_01.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class exercicio_01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        for(int i=0; i<3; i++){
+            System.out.print(scan.nextInt() + "\n");
+        }
+    }
+}
---
+++ nivel-facil/src/exercicio_02.java
@@ -0,0 +1,2 @@
+public class exercicio_02 {
+}
